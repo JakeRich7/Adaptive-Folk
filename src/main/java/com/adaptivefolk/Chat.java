f589bdb841a1ff07656ef7ace7c3d160888e0b97
Jacob Richardson <Jake.Richardson@niceincontact.com>
2026-03-06 19:58:17 -0700

    Added the ability to snag all Kweebec UUIDs when they spawn

--- src/main/java/com/adaptivefolk/Chat.java
+++ src/main/java/com/adaptivefolk/Chat.java
@@ -1,15 +1,17 @@
 package com.adaptivefolk;
 
+import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.event.events.player.PlayerChatEvent;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 
 public class Chat {
-
     public static void EchoPlayer(PlayerChatEvent event) {
-
         String message = event.getContent();
         PlayerRef player = event.getSender();
+        Vector3d playerPos = player.getTransform().getPosition();
+
         player.sendMessage(Message.raw("I am echoing this message: " + message));
+        System.out.println("Player is at: " + playerPos);
     }
 }
--- src/main/java/com/adaptivefolk/PluginEntry.java
+++ src/main/java/com/adaptivefolk/PluginEntry.java
@@ -1,9 +1,19 @@
 package com.adaptivefolk;
 
+import com.hypixel.hytale.common.plugin.PluginManifest;
+import com.hypixel.hytale.component.*;
+import com.hypixel.hytale.component.query.Query;
+import com.hypixel.hytale.component.system.RefSystem;
 import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.event.events.player.PlayerChatEvent;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import com.hypixel.hytale.server.npc.NPCPlugin;
+import com.hypixel.hytale.server.npc.entities.NPCEntity;
+
 
 import javax.annotation.Nonnull;
 
@@ -12,6 +22,11 @@ import javax.annotation.Nonnull;
  * event listeners.
  */
 public class PluginEntry extends JavaPlugin {
+
+    public static final PluginManifest MANIFEST = PluginManifest
+            .corePlugin(PluginEntry.class)
+            .depends(NPCPlugin.class)
+            .build();
 
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
@@ -23,5 +38,48 @@ public class PluginEntry extends JavaPlugin {
     protected void setup() {
         LOGGER.atInfo().log("Setting up plugin " + this.getName());
         this.getEventRegistry().registerGlobal(PlayerChatEvent.class, Chat::EchoPlayer);
+        ComponentRegistryProxy<EntityStore> registry = this.getEntityStoreRegistry();
+        registry.registerSystem(new SpawnFilter());
+    }
+
+    public static class SpawnFilter extends RefSystem<EntityStore> {
+        @Nonnull
+        @Override
+        public Query<EntityStore> getQuery() {
+            return Query.any();
+        }
+
+        @Override
+        public void onEntityAdded(@Nonnull Ref<EntityStore> ref,
+                                  @Nonnull AddReason reason,
+                                  @Nonnull Store<EntityStore> store,
+                                  @Nonnull CommandBuffer<EntityStore> commandBuffer) {
+
+            if (reason != AddReason.SPAWN) {
+                return;
+            }
+
+            NPCEntity npc = (NPCEntity) store.getComponent(ref, NPCEntity.getComponentType());
+            if (npc != null) {
+                String entityType = npc.getRoleName();
+
+                if (entityType.startsWith("Kweebec")) {
+                    TransformComponent transform =
+                            (TransformComponent) store.getComponent(ref, TransformComponent.getComponentType());
+
+                    if (transform != null) {
+                        System.out.println("Spawned " + entityType + " at " + transform.getPosition());
+                        System.out.println(npc);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void onEntityRemove(@Nonnull Ref<EntityStore> ref,
+                                   @Nonnull RemoveReason reason,
+                                   @Nonnull Store<EntityStore> store,
+                                   @Nonnull CommandBuffer<EntityStore> commandBuffer) {
+        }
     }
 }
